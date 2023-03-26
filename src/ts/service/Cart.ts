32d9fda725731a987c82e3d49f0e373c160da279
Mary Kalugina <marina.kalugina9900@gmail.com>
2023-03-26 19:46:50 +0400

    second commit

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -12,19 +12,16 @@ export default class Cart {
     }
 
     count(): number {
-       return this._items.reduce((accumulator, item) => accumulator + item.price, 0);
+       return this._items.reduce((accumulator: number, item: Buyable) => accumulator + item.price, 0);
     }
 
     countWithDiscount(discount: number): number {
-        let totalPrise = this._items.reduce((accumulator, item) => accumulator + item.price, 0);
+        let totalPrise = this.count();
         totalPrise -= (totalPrise / 100) * discount;
         return totalPrise;
     }
 
     deleteItem(id: number): void {
-        let index = this._items.findIndex(item => item.id === id)
-        if (index !== -1) {
-            this._items.splice(index, 1);
-        }
+        this._items = this._items.filter((item: Buyable) => item.id !== id);
     }
 }
